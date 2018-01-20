9a77f1aa19317475a7754b02f8c9f71d77f81806
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2018-01-20 11:16:02 -0600

    latest changes

---
+++ models/bodylang.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var bodylang = {
+//   insert: function(some_val, cb) {
+//     orm.insert("burgers", "burger_name", some_val, function(res) {
+//       cb(res);
+//     });
+//   },
+//   read: function(cb) {
+//     orm.read("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(user_id, cb) {
+//     orm.update("burgers", "devoured", 1, "id", user_id, function(res) {
+//       cb(res);
+//     });
+//   },
+//   delete: function(user_id, cb) {
+//     orm.delete("burgers", "id", user_id, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+
+module.exports = bodylang;
