22db6f9e24f5ab2b388a269ed897f572c0dd8c82
Tanner Martin <twmartin1147@gmail.com>
2018-01-21 11:23:14 -0600

    Added js files for converting text, and background. Updated HTML with NavBar and some body contents

---
+++ public/assets/js/background.js
@@ -0,0 +1,5 @@
+var bgColor = document.getElementById('home-top-first')
+var newColor = element grabbed from table
+function insertEmotion (){
+    bgColor.style.color = newColor
+}
---
+++ public/assets/js/textTranslation.js
@@ -0,0 +1,8 @@
+var emoText = input text of user
+var emotion = returned emotion from Watson
+var textColor = color associated with emotion
+var textFont = font pool assosciated with emotion
+
+funciton textConverter () {
+    if
+}
