6471a5d732fcb7d7241997299e212ee3da5fd5d6
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2018-01-22 17:25:15 -0600

    added export from api, and import from js files

--- api.js
+++ api.js
@@ -1,5 +1,9 @@
 var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
 
+var TextTranslation = require("./public/assets/js/textTranslation.js")
+
+var toneRes = {};
+
 var tone_analyzer = new ToneAnalyzerV3({
   username: "a024676d-1867-42c5-9fac-5908cc52cf02",
   password: "JEK6jBlRLqrN",
@@ -8,7 +12,7 @@ var tone_analyzer = new ToneAnalyzerV3({
 
 tone_analyzer.tone(
   {
-    tone_input: "My charger isn’t working.",
+    tone_input: TextTranslation.emoText,
     content_type: "text/plain"
   },
   function(err, tone) {
@@ -16,6 +20,9 @@ tone_analyzer.tone(
       console.log(err);
     } else {
       console.log(JSON.stringify(tone, null, 2));
+      toneRes = JSON.stringify(tone, null, 2);
     }
   }
 );
+
+Model.export = ToneResults;
