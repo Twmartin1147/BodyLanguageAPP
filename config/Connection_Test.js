7120a86bf55d1855b6978c8cd8da680f5dcd9f61
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2018-01-30 18:43:28 -0600

    sql query testing

---
+++ config/Connection_Test.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bodylangapp_db"
+});
+
+connection.connect();
+
+var watsonRes;
+
+var query = connection.query("SELECT * FROM useremotions WHERE emotions = " + watsonRes);
+
+query.on("error", function(err) {
+  throw err;
+});
+
+query.on("fields", function(fields) {
+  console.log(fields);
+});
+
+query.on("result", function(row) {
+  console.log(row.emotions);
+});
+
+connection.end();
--- public/assets/js/textTranslation.js
+++ public/assets/js/textTranslation.js
@@ -1,11 +1,11 @@
-var db = require("models")
-var ToneResults = require("./../../../api.js")
+var db = require("models");
+var ToneResults = require("./../../../api.js");
 
 // the purpose of this file is to grab the text input by the user, adn apply it to a new ID. 
 // From there, we will grab the row associated with the emotion returned from Watson and apply the CSS
-var emoText = document.getElementById("emotionalText")
-var emoButton = document.getElementById("emoCheck")
-var transText = document.getElementById("translationText")
+var emoText = document.getElementById("emotionalText");
+var emoButton = document.getElementById("emoCheck");
+var transText = document.getElementById("translationText");
 
 
 
@@ -14,7 +14,7 @@ var transText = document.getElementById("translationText")
 // ON click event that applies the users text to another div, then applys the CSS styling grabbed from the DB
 
     $("emoButton").on(click, function (){
-        $("transText").val(emoText)
+        $("transText").val(emoText);
         // <!--$("transText").css()-->
     });
 
@@ -32,7 +32,7 @@ var transText = document.getElementById("translationText")
 
 // fetch("/api/test").then(function(result){
 //     console.log(result)
-})
+// })
 //phase 1 text box that says your emotion
 
-module.export = TextTranslation
+module.export = TextTranslation;
